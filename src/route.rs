002c91ee48e1336be5ff5cb6db4a57bb61ca9461
Krit <suns_stat@hotmail.com>
2023-09-06 00:58:33 +0700

    Add api route

--- src/route.rs
+++ src/route.rs
@@ -15,11 +15,11 @@ use crate::{
 
 pub fn create_router(app_state: Arc<AppState>) -> Router {
     Router::new()
-        .route("/api/healthchecker", get(health_checker_handler))
-        .route("/api/notes", post(create_note_handler))
-        .route("/api/notes", get(note_list_handler))
+        .route("/v1/api/healthchecker", get(health_checker_handler))
+        .route("/v1/api/notes", post(create_note_handler))
+        .route("/v1/api/notes", get(note_list_handler))
         .route(
-            "/api/notes/:id",
+            "/v1/api/notes/:id",
             get(get_note_handler)
                 .patch(edit_note_handler)
                 .delete(delete_note_handler),
